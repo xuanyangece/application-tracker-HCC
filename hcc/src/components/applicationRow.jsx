596afe485604696f7cbe4f2290ad11dbb97a76a1
Xuan Yang <justin@Xuans-MacBook-Air.local>
2019-11-14 11:37:27 -0500

    calendar

--- hcc/src/components/applicationRow.jsx
+++ hcc/src/components/applicationRow.jsx
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import Dropdown from "react-bootstrap/Dropdown";
+import { DatePicker, DatePickerInput } from "rc-datepicker";
+import "rc-datepicker/lib/style.css";
 
 class Row extends Component {
   state = {
     company: "Amazon",
     position: "SDE",
     status: "onsite",
-    date: "X",
+    date: "2015-06-26",
     portal: "URL",
     resume: "URL"
   };
@@ -20,12 +22,16 @@ class Row extends Component {
     margin: "10px auto"
   };
 
+  calStyle = {
+    width: "200px",
+    display: "inline-block"
+  };
+
   render() {
     return (
       <div className="applicationRow" style={this.rowStyle}>
         <p style={this.pStyle}>{this.state.company}</p>
         <p style={this.pStyle}>{this.state.position}</p>
-        <p style={this.pStyle}>{this.state.status}</p>
         <Dropdown style={this.pStyle}>
           <Dropdown.Toggle variant="" id="dropdown-basic">
             Onsite
@@ -37,9 +43,20 @@ class Row extends Component {
             <Dropdown.Item href="#/action-3">Reject</Dropdown.Item>
           </Dropdown.Menu>
         </Dropdown>
+
+        <DatePickerInput
+          onChange={this.onChange}
+          value={this.state.date}
+          className="my-custom-datepicker-component"
+          style={this.calStyle}
+        />
       </div>
     );
   }
+
+  onChange = (jsDate, dateString) => {
+    // ...
+  };
 }
 
 export default Row;
