29d5d313219feef40697dc8d682f824d6869abd7
Xuan Yang <justin@Xuans-MacBook-Air.local>
2019-11-14 11:06:20 -0500

    add button

--- hcc/src/components/applicationRow.jsx
+++ hcc/src/components/applicationRow.jsx
@@ -1,9 +1,40 @@
 import React, { Component } from "react";
+import Dropdown from "react-bootstrap/Dropdown";
 
 class Row extends Component {
-  state = {};
+  state = {
+    company: "Amazon",
+    position: "SDE",
+    status: "onsite",
+    date: "X",
+    portal: "URL",
+    resume: "URL"
+  };
+
+  pStyle = {
+    display: "inline",
+    padding: "5px 25px"
+  };
+
   render() {
-    return <p>This row</p>;
+    return (
+      <div className="applicationRow">
+        <p style={this.pStyle}>{this.state.company}</p>
+        <p style={this.pStyle}>{this.state.position}</p>
+        <p style={this.pStyle}>{this.state.status}</p>
+        <Dropdown style={this.pStyle}>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            Dropdown Button
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+    );
   }
 }
 
