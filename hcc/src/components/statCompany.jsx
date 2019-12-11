61da7872232dbd4253bc3a0d036ab7367cfdae38
Xuan Yang <justin@Xuans-MacBook-Air.local>
2019-12-11 14:40:04 -0500

    Add stat company page

---
+++ hcc/src/components/statCompany.jsx
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+import "../styles/styles.css";
+import { Link } from "react-router-dom";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { Checkbox } from "semantic-ui-react";
+
+class timeline extends Component {
+  state = {
+    refered: false
+  };
+
+  titleStyle = {
+    paddingLeft: "0px",
+    paddingRight: "0px"
+  };
+  render() {
+    return (
+      <div>
+        <Container className="newappform">
+          <p className="titles">Select Company Name:</p>
+          <Checkbox label="Google" />
+          <Checkbox label="Facebook" />
+          <Checkbox label="Nike" />
+          <Checkbox label="Amazon" />
+        </Container>
+
+        <Container className="newappbuttons">
+          {this.renderAddButton()}
+          {this.renderGoBackButton()}
+        </Container>
+      </div>
+    );
+  }
+
+  renderAddButton = () => {
+    return (
+      <Link to="/addednewapp">
+        <button className="btn-primary btn-lg m-1">Select Position</button>
+      </Link>
+    );
+  };
+
+  renderGoBackButton = () => {
+    return (
+      <Link to="/">
+        <button className="btn-primary btn-lg m-1">Back To Dashboard</button>
+      </Link>
+    );
+  };
+
+  //   renderUpload = () => {
+  //     return <button className="btn-primary btn-sm m-2">Upload</button>;
+  //   };
+
+  //   toggleRefered = prevState => {
+  //     this.setState(prevState => ({ refered: !prevState.refered }));
+  //   };
+
+  //   renderReferButton = () => {
+  //     return this.state.refered ? (
+  //       <button onClick={this.toggleRefered} className="btn-primary btn-sm m-2">
+  //         Yes
+  //       </button>
+  //     ) : (
+  //       <button onClick={this.toggleRefered} className="btn-warning btn-sm m-2">
+  //         No
+  //       </button>
+  //     );
+  //   };
+}
+
+export default timeline;
--- hcc/src/index.js
+++ hcc/src/index.js
@@ -10,6 +10,7 @@ import timeline from "./components/timeline";
 import updateUpcomingDate from "./components/updateUpcomingDate";
 import waitToUpcoming from "./components/updateWaitToUpcoming";
 import waitToDone from "./components/updateWaitToDone";
+import statCompany from "./components/statCompany";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 const app = (
@@ -22,6 +23,7 @@ const app = (
       <Route path="/updateupcomingdate" exact component={updateUpcomingDate} />
       <Route path="/waittoupcoming" exact component={waitToUpcoming} />
       <Route path="/waittodone" exact component={waitToDone} />
+      <Route path="/statcompany" exact component={statCompany} />
     </Switch>
   </BrowserRouter>
 );
